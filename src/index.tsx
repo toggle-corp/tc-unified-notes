bbe383956c4cf9f827b76a106e1acac3e164833f
Shreeyash Shrestha <45554490+shreeyash07@users.noreply.github.com>
2025-01-09 14:50:44 +0545

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App/index.tsx';
+
+const webappRootId = 'webapp-root';
+
+const webappRootElement = document.getElementById(webappRootId);
+
+if (!webappRootElement) {
+    // eslint-disable-next-line no-console
+    console.error(`Could not find html element with id '${webappRootId}'`);
+} else {
+    ReactDOM.createRoot(webappRootElement).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    );
+}
---
+++ vite.config.ts
@@ -0,0 +1,59 @@
+import { ValidateEnv as validateEnv } from '@julr/vite-plugin-validate-env';
+import reactSwc from '@vitejs/plugin-react-swc';
+import { execSync } from 'child_process';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import { compression } from 'vite-plugin-compression2';
+import svgr from 'vite-plugin-svgr';
+import webfontDownload from 'vite-plugin-webfont-dl';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import envConfig from './env';
+
+/* Get commit hash */
+const commitHash = execSync('git rev-parse --short HEAD').toString();
+
+export default defineConfig(({ mode }) => {
+    const isProd = mode === 'production';
+    return {
+        define: {
+            APP_COMMIT_HASH: JSON.stringify(commitHash),
+        },
+        plugins: [
+            isProd ? checker({
+                typescript: true,
+                eslint: {
+                    lintCommand: 'eslint ./src',
+                },
+                stylelint: {
+                    lintCommand: 'stylelint "./src/**/*.css"',
+                },
+            }) : undefined,
+            svgr(),
+            reactSwc(),
+            tsconfigPaths(),
+            webfontDownload(),
+            validateEnv(envConfig),
+            isProd ? compression() : undefined,
+        ],
+        css: {
+            devSourcemap: isProd,
+            modules: {
+                scopeBehaviour: 'local',
+                localsConvention: 'camelCaseOnly',
+            },
+        },
+        envPrefix: 'APP_',
+        server: {
+            port: 3000,
+            strictPort: true,
+        },
+        build: {
+            outDir: 'build',
+            sourcemap: isProd,
+        },
+        test: {
+            environment: 'happy-dom',
+        },
+    };
+});
